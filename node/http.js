0a8806380b8abf88d7e8478951ce5a608c6ca033
hjxool <691638334@qq.com>
2023-03-21 17:55:45 +0800

    3.21

--- node/http.js
+++ node/http.js
@@ -1,9 +1,10 @@
-const http = require("http");
+const http = require('http');
 
 const server = http.createServer((request, response) => {
-	response.end("收到收到");
+	response.setHeader('content-type', 'text/html;charset=utf-8');
+	response.end('收到收到,roger that');
 });
 
 server.listen(9000, () => {
-	console.log("服务已启动...");
+	console.log('服务已启动...');
 });
