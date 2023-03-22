3a4c1676db599a4ba5921d7a17d6830b9586b188
hjxool <691638334@qq.com>
2023-03-22 17:50:59 +0800

    3.22

--- node/http.js
+++ node/http.js
@@ -1,8 +1,61 @@
 const http = require('http');
 
+// const server = http.createServer((request, response) => {
+// 	let body = '';
+// 	request.on('data', (chunk) => {
+// 		console.log('片数据', chunk.toString());
+// 		body += chunk;
+// 	});
+// 	request.on('end', () => {
+// 		console.log('读取完毕', body);
+// 		response.setHeader('content-type', 'text/html;charset=utf-8');
+// 		response.end('收到收到,roger that');
+// 	});
+// });
+
+// const url = require('url');
+// const server = http.createServer((request, response) => {
+// 	let res = url.parse(request.url, true);
+// 	console.log(res.query);
+// });
+
+// const server = http.createServer((request, response) => {
+// 	let url = new URL(request.url, 'http://127.0.0.1');
+// 	console.log('url', url);
+// 	console.log(url.searchParams.getAll('name'));
+// });
+
+// const server = http.createServer((request, response) => {
+// 	let { method, url } = request;
+// 	let { pathname } = new URL('http://127.0.0.1' + url);
+// 	response.setHeader('content-type', 'text/html;charset=utf-8');
+// 	if (method == 'GET' && pathname == '/login') {
+// 		response.end('登录页');
+// 	} else if (method == 'GET' && pathname == '/reg') {
+// 		response.end('注册页');
+// 	} else {
+// 		response.end('404 NOT FOUND');
+// 	}
+// });
+
+// const server = http.createServer((request, response) => {
+// 	response.statusCode = 404;
+// 	response.end('response');
+// });
+
+const fs = require('fs');
 const server = http.createServer((request, response) => {
-	response.setHeader('content-type', 'text/html;charset=utf-8');
-	response.end('收到收到,roger that');
+	let { pathname } = new URL(`http://127.0.0.1${request.url}`);
+	if (pathname == '/login') {
+		let html = fs.readFileSync('./http.html');
+		response.write(html);
+		response.end();
+	} else {
+		// response.statusCode = 404;
+		// response.end('Not Found hhh');
+		let f = fs.readFileSync(`.${pathname}`);
+		response.end(f);
+	}
 });
 
 server.listen(9000, () => {
