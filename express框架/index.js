29c0edd57a9d033b2a7841d62a074276d78dd47b
hjxool <691638334@qq.com>
2023-04-04 18:03:28 +0800

    4.4

--- express框架/index.js
+++ express框架/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
-app.get('/home', (req, res) => {
-	console.log(req, res);
-	res.setHeader('content-type', 'text/html;charset=utf-8');
-	res.end('收到');
+// app.all('*', (req, res) => {
+// 	console.log(req);
+// 	res.end('404');
+// });
+app.get('/:id1.:id2.html', (req, res) => {
+	// res.setHeader('content-type', 'text/html;charset=utf-8');
+	console.log(req.params);
+	res.send('收到');
 });
-app.get('/', (req, res) => {
-	res.end('end');
-});
+// app.get('/', (req, res) => {
+// 	res.end('end');
+// });
+
 app.listen('3000', () => {
 	console.log('服务已启动');
 });
