0ea03bf993928dad23fbd02a4510577ed8c59bb8
hjxool <691638334@qq.com>
2023-04-06 18:01:32 +0800

    4.6

--- express框架/index.js
+++ express框架/index.js
@@ -1,20 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
 // app.all('*', (req, res) => {
 // 	console.log(req);
 // 	res.end('404');
 // });
-app.get("/:id1/:id2.html", (req, res) => {
-	// res.setHeader('content-type', 'text/html;charset=utf-8');
-	console.log(req.params);
-	res.send(JSON.stringify(req.params));
-});
+
+// app.get("/:id1/:id2.html", (req, res) => {
+// res.setHeader('content-type', 'text/html;charset=utf-8');
+// console.log(req.params);
+// res.send(JSON.stringify(req.params));
+// });
+
 // app.get('/', (req, res) => {
 // 	res.end('end');
 // });
 
-app.listen("3000", () => {
-	console.log("服务已启动");
+app.all('/', (req, res) => {
+	console.log('收到请求');
+	// res.redirect('./login');
+	res.download('./test.txt', '新名字.txt');
 });
-const t = require("./test.js");
-console.log(t);
+app.all('/login', (req, res) => {
+	// res.send(`console.log('login')`);
+	// res.download('./package.json');
+});
+
+app.listen('5500', () => {
+	console.log('5500端口监听');
+});
+// const t = require('./test.js');
+// console.log(t);
