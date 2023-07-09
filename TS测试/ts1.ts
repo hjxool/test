ea3787f64e9d5c7211f4d3e91382aa1d6d8fbc67
houjie <691638334@qq.com>
2023-07-09 17:58:32 +0800

    7.9

---
+++ TS测试/ts1.ts
@@ -0,0 +1,20 @@
+// 测试 类型不一致会如何 答：会提示报错，但依然会生成js文件
+// let a: number
+// a = 'hhh'
+
+// 测试 函数类型声明
+// function fn(a: number, b: number) {
+//   return a + b
+// }
+// console.log(fn(22, 10))
+
+// 测试 unknow类型可以赋值unknow类型吗？ 答：可以
+// let a: unknown = 10
+// let b: unknown
+// b = a
+// console.log(b)
+
+// 测试 unknow类型可以赋值any类型吗？ 答：可以
+let a: unknown = 10
+let b
+b = a
