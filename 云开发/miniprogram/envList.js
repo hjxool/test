178aa7178e0b2df0ba6c8e238a2cb44bb3ea59a8
hjxool <691638334@qq.com>
2023-08-19 22:17:22 +0800

    8.19

---
+++ 云开发/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-0gzy726e39ba4d96","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ 云开发/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\D\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-0gzy726e39ba4d96 --n quickstartFunctions --r --project "D:\G\Git仓库\test\云开发" --report_first --report
