09e4560c31c1ac95a9531b45350efeeb46cb2548
houjie <691638334@qq.com>
2024-01-12 18:04:41 +0800

    1.12

--- hhymDemo/miniprogram/pages/order/order.js
+++ hhymDemo/miniprogram/pages/order/order.js
@@ -2,15 +2,35 @@ Page({
   data: {
     list: [], //订单列表
     select_date: "", //时间筛选
+    select_user: "", // 用户筛选
+    user_list: [], // 用户列表
+    select_status: "", // 订单状态
+    order_status: [
+      { label: "已确认", value: 1 },
+      { label: "待确认", value: 0 },
+      { label: "已过期", value: 2 },
+      { label: "已取消", value: -1 },
+    ],
   },
   async onLoad(options) {
-    this.app = getApp()
+    this.app = getApp();
+    // 查询用户列表
+    await this.get_users();
     // 每次打开查询全部订单
-    await this.get_data()
+    // await this.get_data();
   },
-  select_time(e) {
+  // 选择时间
+  select_value(e) {
+    let { tag } = e.currentTarget.dataset;
     this.setData({
-      select_date: e.detail.value,
+      [tag]: e.detail.value,
+    });
+  },
+  // 选择器点取消清除值
+  clear_value(e) {
+    let { tag } = e.currentTarget.dataset;
+    this.setData({
+      [tag]: "",
     });
   },
   // 生成房间文字
@@ -36,9 +56,20 @@ Page({
     return `${t.getFullYear()}.${t.getMonth() + 1}.${t.getDate()}`;
   },
   // 获取订单列表
-  async get_data(){
-    this.app.mycall('get_orders_by_paging',{
-      
-    })
-  }
+  async get_data() {
+    let body = {};
+    this.app.mycall("get_orders_by_paging", body);
+  },
+  // 查询用户列表
+  async get_users() {
+    let { data: res } = await this.app.mycall("customer", {
+      type: "get",
+      condition: {},
+    });
+    if (res) {
+      this.setData({
+        user_list: res,
+      });
+    }
+  },
 });
