912125d3edb018ad85db03794781d95486d5743c
hjxool <691638334@qq.com>
2024-02-26 23:17:34 +0800

    2.26

--- hhymDemo/miniprogram/pages/calendar/calendar.js
+++ hhymDemo/miniprogram/pages/calendar/calendar.js
@@ -5,29 +5,19 @@ Page({
     weeks: ["日", "一", "二", "三", "四", "五", "六"], //星期
     date_list: [], // 日期列表
     date_boundary: 0, // 当天之前的所有天数全部灰掉
+    pop_show: false, // 显示弹窗
+    day_user_order: [], // 当日用户以及订单列表
   },
   onLoad() {
-    // 一加载就生成当月及下月日期列表
-    this.init_day_list();
+    this.app = getApp()
     this.channel = this.getOpenerEventChannel();
     this.channel.on("customer_list", (data) => {
+      // 在拿到数据后再渲染日期列表
+      // 因为count_day只会在初次生成单月列表时调用
+      // 利用这点可以避免每次都要循环遍历总日期列表 而且省去了验证每个订单在每月每日中唯一性
       // 遍历订单列表 往date_list上添加样式及详细数据
-      for (let o of data) {
-        for (let d of this.data.date_list) {
-          // 先根据已生成日期列表找处在哪个月
-          if (o.start >= d.month_date.start && o.start <= d.month_date.end) {
-            // 处在当前月 则遍历当月每一天时间戳 找到对应天
-            for(let d2 of d.days){
-              // 为了避免重复添加 要验证是否已经存在相同订单
-              if (d2.date === o.start) {
-                
-                break
-              }
-            }
-            break
-          }
-        }
-      }
+      this.orders = data;
+      this.init_day_list();
     });
   },
   // 生成当月和下月的日期列表
@@ -92,6 +82,7 @@ Page({
         text: index, // 日期文字
         orders: [], // 存当天的订单列表
       };
+
       t.days.push(d2);
     }
     // 计算剩余天数 剩余天数用空格
@@ -101,6 +92,20 @@ Page({
         date: 0,
         text: "",
       });
+    }
+    // 返回前根据订单列表 将当月的订单加到对应天里
+    for (let o of this.orders) {
+      if (o.start >= t.month_date.start && o.start <= t.month_date.end) {
+        // 只遍历中间有日期的
+        for (let i = cur_week; i < total_day + cur_week; i++) {
+          let ii = t.days[i];
+          if (ii.date === o.start) {
+            ii.orders.push(o);
+            // 一个订单只会对应一天 当找到对应的日期 就应该跳过剩余的天数
+            break;
+          }
+        }
+      }
     }
     return t;
   },
@@ -122,5 +127,93 @@ Page({
     this.setData({
       date_list: list,
     });
+  },
+  // 查看当天的订单信息
+  async check(e) {
+    let { orders } = e.currentTarget.dataset;
+    // 没有订单不继续执行
+    if (!orders.length) {
+      return;
+    }
+    wx.showLoading({
+      title: "加载中",
+      mask: true,
+    });
+    // 查询全部用户
+    let { data: res } = await this.app.mycall("customer", {
+      type: "get",
+      condition: {},
+    });
+    wx.hideLoading();
+    if (res) {
+      let list = [];
+      for (let val of res) {
+        // 因为要把用户下所有当日订单找出来 所以每一个订单都要遍历
+        for (let val2 of orders) {
+          if (val._id === val2.customer_id) {
+            // 找到当日订单中的用户后 查看是否在list中已经存在
+            // 存在则往用户所属订单下填充 不存在则创建用户信息及初始订单
+            let find = false;
+            for (let val3 of list) {
+              if (val3.id === val._id) {
+                find = val3.orders;
+                break;
+              }
+            }
+            let et = new Date(val2.end);
+            let child = {
+              end_to: `~ ${et.getFullYear()}.${
+                et.getMonth() + 1
+              }.${et.getDate()}`,
+              pet_name: val2.pet_name.join("、"),
+              cost: val2.cost,
+              room: this.format_room_text(val2.room),
+            };
+            if (!find) {
+              let t = {
+                id: val._id,
+                name: val.name,
+                phone: val.phone,
+                weChat: val.weChat,
+                orders: [child],
+              };
+              list.push(t);
+            } else {
+              find.push(child);
+            }
+          }
+        }
+      }
+      this.setData({
+        pop_show: true,
+        day_user_order: list,
+      });
+    }
+  },
+  // 复制一行内容到剪贴板
+  async copy_content(e) {
+    let data = e.currentTarget.dataset.content;
+    await wx.setClipboardData({ data });
+    wx.showToast({ title: "已复制到剪贴板", icon: "none" });
+    setTimeout(() => {
+      wx.hideToast();
+    }, 1000);
+  },
+  // 生成房间文字
+  format_room_text(room_id) {
+    switch (room_id) {
+      case "3":
+        return "豪华间1";
+      case "11":
+        return "豪华间2";
+      default:
+        if (room_id == "1" || room_id == "2") {
+          return `标准间${room_id}`;
+        } else if (Number(room_id) >= 4 && Number(room_id) <= 10) {
+          return `标准间${Number(room_id) - 1}`;
+        } else if (room_id == "12" || room_id == "13") {
+          return `标准间${Number(room_id) - 2}`;
+        }
+    }
   },
 });
