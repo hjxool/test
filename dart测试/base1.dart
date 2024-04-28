9e51d8e70b0004ddeab87e677a9758f56cd89bd8
houjie <691638334@qq.com>
2024-04-28 18:01:24 +0800

    4.28

--- dart测试/base1.dart
+++ dart测试/base1.dart
@@ -7,5 +7,19 @@ main() {
   // 转换成整数
   // print(5.5.toInt());
   // 四舍五入
-  print(-3.7.round());
+  // print(-3.7.round());
+
+  // 字符串类型
+  String str = '''
+  Hello
+  World
+  ''';
+  // print(str);
+  String str2 = '你好';
+  // 字符串拼接
+  // print(str + str2);
+  // print('$str2 $str');
+  // 字符串分隔
+  // print(str2.split(''));
+  print('qwe'.split(''));
 }
